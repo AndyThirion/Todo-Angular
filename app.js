c94e4b6eeaad9db9141a595ed453e9e7dc1666df
AndyThirion <andy.thirion@gmail.com>
2015-03-13 16:51:00 -0500

    Funct: Add, track, remove

--- app.js
+++ app.js
@@ -3,13 +3,30 @@
 
 	app.controller('TodoCtrl', function(){
 		this.list = [
-			{text: "Brush Teeth"},
-			{text: "Take Medicine"},
-			{text: "Check Laundry"}
+			{text: "Brush Teeth", order: 0},
+			{text: "Take Medicine", order: 1},
+			{text: "Check Laundry", order: 2}
 		];
-		this.addTodo = function(todoText){
-			this.list.push({text: todoText});
+		this.newTodo = function(text, order){
+			this.text = text;
+			this.order = order || -1;
+			this.isComplete = false;
+		},
+		this.addTodo = function(text, order) {
+			var tmpTodo = new this.newTodo(text, order);
+			console.log("tmp", tmpTodo)
+			this.list.push(tmpTodo);
+			$('#todoInput').val('');
+		},
+
+		this.removeTodo = function(order){
+			this.list.splice(order, 1);
 			console.log(this.list);
+
+			for (var i=0; i<this.list.length; i++)
+			{
+				this.list[i].order = i;
+			}
 		}
 
 		this.helloWorld = "Hello Angular!"
